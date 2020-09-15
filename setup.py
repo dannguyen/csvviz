10a3a6bc3bb8277ca897d6ca5123c2ce825ac367
Dan Nguyen <dansonguyen@gmail.com>
2020-09-15 16:55:47 -0500

    setup fix

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dannguyen/csvviz',
-    version='0.1.0-alpha',
+    version='0.1.1-alpha',
     zip_safe=False,
 )
