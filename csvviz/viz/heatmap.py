740ee47d8a39d6026ab46e3056f2aa5240b82b51
Dan Nguyen <dansonguyen@gmail.com>
2020-11-11 18:43:42 -0600

    heatmap bare implementation

---
+++ csvviz/viz/heatmap.py
@@ -0,0 +1,63 @@
+"""
+heatmap.py
+
+
+csvviz heatmap -x state -y item -c sold examples/hot.csv
+
+"""
+
+import altair as alt
+import click
+from csvviz.exceptions import *
+from csvviz.vizkit import Vizkit
+
+class Heatmapkit(Vizkit):
+
+    viz_commandname = "heatmap"
+    viz_info = f"""A TKTKTK"""
+    viz_epilog = """Example:\t csvviz heatmap -x state -y item -c sold examples/hot.csv"""
+
+
+
+    COMMAND_DECORATORS = (
+        click.option(
+            "--xvar",
+            "-x",
+            type=click.STRING,
+            help="The name of the column for mapping x-axis values; if empty, the first (columns[0]) column is used",
+        ),
+        click.option(
+            "--yvar",
+            "-y",
+            type=click.STRING,
+            help="The name of the column for mapping y-axis values; if empty, the second (columns[1]) column is used",
+        ),
+        click.option(
+            "--colorvar",
+            "-c",
+            "fillvar",
+            type=click.STRING,
+            help="The name of the column for mapping TK colors.",
+        ),
+
+        # # https://altair-viz.github.io/user_guide/encoding.html#sorting
+        # click.option(
+        #     "--x-sort",
+        #     "-xs",
+        #     "sortx_var",
+        #     type=click.STRING,
+        #     help="Sort the x-axis by the values of the x/y/fill channel. Prefix with '-' to do reverse sort, e.g. 'y' vs '-y'",
+        # ),
+
+    )
+
+
+    def finalize_channels(self, channels):
+        self._set_channel_colorscale("fill", channels)
+
+        if not channels.get("fill"):
+            raise MissingDataReference(
+                "-c/--colorvar needs to be specified"
+            )
+
+        return channels
---
+++ tests/unit/viz/test_heatmap.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import pytest
+from click.testing import CliRunner
+
+import json
+from pathlib import Path
+
+from csvviz.exceptions import *
+from csvviz.settings import *
+
+from csvviz.viz.heatmap import Heatmapkit
+
+heatmap = Heatmapkit.register_command()
+
+
+
+HOT_ARGS = [
+    "examples/hot.csv",
+    "--json",
+    "--no-preview",
+]
+
+
+def test_heatmap_defaults():
+    c = CliRunner().invoke(heatmap,
+        [   '-x', 'state',
+            '-y', 'item',
+            '-c', 'sold',
+            *HOT_ARGS,
+        ]
+    )
+
+    cdata = json.loads(c.output)
+
+    assert cdata["mark"]["type"] == "rect"
+
+    datavals = list(cdata["datasets"].values())[0]
+    assert datavals[0] ==       {
+        "item": "apples",
+        "sold": 80,
+        "state": "CA"
+      }
+
+    # TODO: test fill encoding
+
+
+
